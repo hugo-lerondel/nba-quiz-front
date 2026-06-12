4fcff6df83c5a81cc32beb2bc9909ebea4ca474f
hugo.lerondel <hlerondel@takima.fr>
2026-06-12 19:30:00 +0200

    feat: change logo and name

--- vite.config.ts
+++ vite.config.ts
@@ -6,29 +6,37 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "/nba-quiz-front/" ? "/cv/" : "/",
+	base: process.env.NODE_ENV === "production" ? "/hoop-quiz/" : "/",
 	plugins: [
 		react(),
 		tailwindcss(),
 		VitePWA({
 			registerType: "autoUpdate",
 			manifest: {
-				name: "NBA Quiz",
-				short_name: "NBA Quiz",
+				name: "HoopQuiz",
+				short_name: "HoopQuiz",
 				description: "Application de quiz NBA",
-				theme_color: "#1d428a",
-				background_color: "#ffffff",
+				theme_color: "#08080f",
+				background_color: "#08080f",
 				display: "standalone",
 				icons: [
 					{
 						src: "pwa-192x192.png",
 						sizes: "192x192",
 						type: "image/png",
+						purpose: "any",
 					},
 					{
 						src: "pwa-512x512.png",
 						sizes: "512x512",
 						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "pwa-512x512-maskable.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
 					},
 				],
 			},
