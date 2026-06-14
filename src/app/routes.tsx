35c07237c72a9057495bc2fdad0a97cb1feabd45
hugo.lerondel <hlerondel@takima.fr>
2026-06-14 22:44:40 +0200

    fix: prod

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -7,18 +7,21 @@ import { SimpleQuizPage } from "./pages/quiz/SimpleQuizPage";
 import { WhoAmIGamePage } from "./pages/whoami/WhoAmIGamePage";
 import { WhoAmIListPage } from "./pages/whoami/WhoAmIListPage";
 
-export const router = createBrowserRouter([
-	{
-		path: "/",
-		Component: () => <Outlet />,
-		children: [
-			{ index: true, Component: HomePage },
-			{ path: "enumeration", Component: EnumerationListPage },
-			{ path: "enumeration/:id", Component: EnumerationQuizPage },
-			{ path: "quiz", Component: QuizListPage },
-			{ path: "quiz/:id", Component: SimpleQuizPage },
-			{ path: "whoami", Component: WhoAmIListPage },
-			{ path: "whoami/:id", Component: WhoAmIGamePage },
-		],
-	},
-]);
+export const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			Component: () => <Outlet />,
+			children: [
+				{ index: true, Component: HomePage },
+				{ path: "enumeration", Component: EnumerationListPage },
+				{ path: "enumeration/:id", Component: EnumerationQuizPage },
+				{ path: "quiz", Component: QuizListPage },
+				{ path: "quiz/:id", Component: SimpleQuizPage },
+				{ path: "whoami", Component: WhoAmIListPage },
+				{ path: "whoami/:id", Component: WhoAmIGamePage },
+			],
+		},
+	],
+	{ basename: import.meta.env.BASE_URL },
+);
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "production" ? "/nba-quiz-front/" : "/",
+	base: "/nba-quiz-front/",
 	plugins: [
 		react(),
 		tailwindcss(),
