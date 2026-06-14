2b8c6fc759f9cf2c18a1c414530a4eae88325353
hugo.lerondel <hlerondel@takima.fr>
2026-06-14 21:41:38 +0200

    fix: prod

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "production" ? "/hoop-quiz/" : "/",
 	plugins: [
 		react(),
 		tailwindcss(),
